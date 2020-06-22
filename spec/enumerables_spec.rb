c676e194c27a2f8a0403e67bea546920a739c005
Shingirayi Mandebvu <shingim1@gmail.com>
2020-06-22 18:53:10 +0200

    Add tests for #my_all

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -5,6 +5,8 @@ describe Enumerable do
   let(:arr) { Array(1..5) }
   let(:hash) { Hash.new }
   let(:arr_words) { %w(cat dog wombat) }
+  let(:arr_with_ts) { %w(ant bat cat) }
+
   describe "#my_each" do
     it 'If no block is given, an Enumerator is returned.' do
       expect(arr.my_each).not_to eql(arr)
@@ -42,6 +44,23 @@ describe Enumerable do
     end
     it 'Must throw argument error if given arguments ().' do
       expect { arr.my_select(9) }.to raise_error(ArgumentError)
+    end
+  end
+  describe "#my_all" do 
+    it "Must return true if given regular expression returns true." do 
+      expect(arr_with_ts.my_all?(/t/)).to eql(true)
+    end
+    it "Must return true if given class returns true." do 
+      expect(arr.my_all?(Integer)).to eql(true)
+    end
+    it "Must return true if no block is given." do
+      expect(arr_words.my_all?).not_to eql(false)
+    end 
+    it "Must give same result as all? when block given. " do 
+      expect(arr.my_all?{|value| value >= 2 }).to eql(arr.all?{|value| value >= 2 })
+    end
+    it "Must use an argument instead of block when given an argument and a block." do
+      expect(arr.my_all?(/t/) { |v| v >= 0 }).not_to eql(true)
     end
   end
   
